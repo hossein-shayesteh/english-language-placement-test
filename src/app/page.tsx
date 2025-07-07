200addfb9914abce3a2898ec479b7432d4e134e4
Hossein Shayesteh <hosseinxshayesteh@gmail.com>
2025-07-07 16:03:43 +0330

    feat: Add FLL logo to SignInPage and implement sign out functionality in Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,22 @@
-import { auth } from "@/auth";
+import { auth, signOut } from "@/auth";
 
 const Home = async () => {
   const session = await auth();
 
   console.log(session?.user);
-  return <div>Home</div>;
+  return (
+    <>
+      <div>Home</div>
+      <form
+        action={async () => {
+          "use server";
+          await signOut();
+        }}
+      >
+        <button>sign out</button>
+      </form>
+    </>
+  );
 };
 
 export default Home;
